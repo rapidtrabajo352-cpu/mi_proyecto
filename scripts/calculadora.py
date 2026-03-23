4733e9498ee8f5443fddc80f7bbd0293e6cd2e6c
rapidtrabajo352 <rapidtrabajo352@gmail.com>
2026-03-23 02:12:23 -0500

    Agregué una calculadora básica

---
+++ scripts/calculadora.py
@@ -0,0 +1,7 @@
+print("--- MI CALCULADORA ---")
+num1 = float(input("Escribe el primer número: "))
+num2 = float(input("Escribe el segundo número: "))
+
+suma = num1 + num2
+
+print("La suma de " + str(num1) + " y " + str(num2) + " es: " + str(suma))
